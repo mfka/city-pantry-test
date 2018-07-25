8954327bfe37bc6ef63e171f24e73a42114b0b65
Marek Firlejczyk <marek.firlejczyk@rst-it.com>
2018-07-25 12:34:13 +0200

    Improve Tests

--- tests/Handler/SearchHandlerTest.php
+++ tests/Handler/SearchHandlerTest.php
@@ -4,49 +4,49 @@ namespace App\Test\Handler;
 
 use App\Command\SearchCommand;
 use App\Handler\SearchHandler;
+use App\Service\VendorsFileProcessor;
 use Mockery as m;
 use PHPUnit\Framework\TestCase;
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
-use Symfony\Component\Validator\ConstraintViolationList;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\Exception\ValidatorException;
 use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 class SearchHandlerTest extends TestCase
 {
-    private $dispatcher;
-    private $validator;
-
-    public function setUp(): void
-    {
-        $this->dispatcher = m::mock(EventDispatcherInterface::class);
-        $this->validator = m::mock(ValidatorInterface::class);
-        $handler = new SearchHandler($this->validator, $this->dispatcher);
-    }
 
     public function testHandle(): void
     {
-
-
+        $processor = m::mock(VendorsFileProcessor::class);
+        $processor->shouldReceive('process');
+        $validator = m::mock(ValidatorInterface::class);
+        $validator->shouldReceive('validate')->andReturn([]);
+        $handler = new SearchHandler($validator, $processor);
+        $handler->handle($this->createSearchCommand());
         $this->addToAssertionCount(1);
     }
 
-    public function testValidate(): void
+    public function testValidateError(): void
     {
+        $processor = m::mock(VendorsFileProcessor::class);
         $validator = m::mock(ValidatorInterface::class);
-        $command = $this->createSearchCommand();
-        $errors = new ConstraintViolationList();
-        $validator->shouldReceive('validate')->with($command)->andReturn($errors);
-
+        $error = new ConstraintViolation('error','message {{template}}', [],'root', 'ErrorTest','value');
+        $validator->shouldReceive('validate')->andReturn([$error]);
+        $handler = new SearchHandler($validator, $processor);
+        $this->expectException(ValidatorException::class);
+        $handler->handle($this->createSearchCommand());
     }
 
-    private function createSearchCommand()
+    private function createSearchCommand(): SearchCommand
     {
-        return new SearchCommand(
+        $command = new SearchCommand(
             __DIR__.'/../examples/example-input.txt',
             '11/12/20',
             '23:59',
             'NW43QB',
             2
         );
+
+        return $command;
     }
 
     private function createIncorrectSearchCommand()
